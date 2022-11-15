8191ff462e8da37a6bd28bf72b4cfdb16e9de93b
unknown <wgmlgz.god@gmail.com>
2022-11-15 14:40:38 +0300

    better regex

--- grab-links.py
+++ grab-links.py
@@ -8,6 +8,7 @@ import os
 from collections import deque
 import asyncio
 
+
 async def get_page():
     browser = await launch()
     page = await browser.newPage()
@@ -15,69 +16,62 @@ async def get_page():
     return page, browser
 
 
+async def grab(pattern: re.Pattern[str], full_pattern: re.Pattern[str], folder: str):
+    base = 'https://www.ozon.ru'
+    page, browser = await get_page()
+
+    q = deque()
+    found = set()
+    used = set()
+
+    def grab_categories(page_content):
+        soup = BeautifulSoup(page_content)
+        links = set()
+        for a in soup.find_all('a', href=True):
+            if re.fullmatch(pattern, a['href']) is not None:
+                link = base + a['href']
+                if link in found:
+                    continue
+
+                links.add(link)
+                found.add(link)
+
+        return links
+
+    used = set([i.strip() for i in open(folder + '/used.txt',
+               'r').readlines() if re.fullmatch(full_pattern, i.strip()) is not None])
+    found = set([i.strip() for i in open(folder + '/found.txt',
+                'r').readlines() if re.fullmatch(full_pattern, i.strip()) is not None])
+    q.extend([i.strip() for i in open(folder + '/queue.txt',
+             'r').readlines() if re.fullmatch(full_pattern, i.strip()) is not None])
+
+    count = 0
+
+    while len(q) != 0:
+        url = q.popleft()
+        if url in used:
+            continue
+        used.add(url)
+        try:
+            await page.goto(url, {'waitUntil': 'networkidle0'})
+            page_content = await page.content()
+            links = grab_categories(page_content)
+            for i in links:
+                q.append(i)
+            print('done', count)
+
+            count += 1
+            if count % 1 == 0:
+                open(folder + '/used.txt', 'w+').write('\n'.join(used))
+                open(folder + '/found.txt', 'w+').write('\n'.join(found))
+                open(folder + '/queue.txt', 'w+').write('\n'.join(q))
+                print('write')
+        except:
+            print('timeout error!')
+            q.extend(url)
+            await browser.close()
+            page, browser = await get_page()
 
 
-
-
-
-
- 
-async def grab(pattern: re.Pattern[str], folder: str):
-  base = 'https://www.ozon.ru'
-  page, browser = await get_page()
-  
-  queue_categories = deque()
-  found_categories = set()
-  used_categories = set()
-  
-  def grab_categories(page_content):
-    soup = BeautifulSoup(page_content)
-    links = set()
-    for a in soup.find_all('a', href=True):
-        if re.fullmatch(pattern, a['href']) is not None:
-            link = base + a['href']
-            if link in found_categories:
-                continue
-
-            links.add(link)
-            found_categories.add(link)
-
-    return links
-
-  used_categories = set(map(lambda s: s.strip(), filter(None, open(
-      folder + '/used.txt', 'r').readlines())))
-  found_categories = set(map(lambda s: s.strip(), filter(None, open(
-      folder + '/found.txt', 'r').readlines())))
-  queue_categories.clear()
-  queue_categories.extend(map(lambda s: s.strip(), filter(None, open(
-      folder + '/queue.txt', 'r').readlines())))
-
-  count = 0
-
-  while len(queue_categories) != 0:
-      url = queue_categories.pop()
-      if url in used_categories:
-          continue
-      used_categories.add(url)
-      try:
-          await page.goto(url, {'waitUntil': 'networkidle0'})
-          page_content = await page.content()
-          links = grab_categories(page_content)
-          queue_categories.extend(list(links))
-          print('done', count)
-          
-          count += 1
-          if count % 3 == 0:
-            open(folder + '/used.txt', 'w+').write('\n'.join(used_categories), )
-            open(folder + '/found.txt', 'w+').write('\n'.join(found_categories))
-            open(folder + '/queue.txt', 'w+').write('\n'.join(queue_categories))
-            print('write')
-      except:
-          print('timeout error!')
-          queue_categories.extend(url)
-          await browser.close()
-          page, browser = await get_page()
-          
-
-
-asyncio.run(grab(re.compile('\/category\/[^\/]+\\d+\/'), './parse/categories'))
+asyncio.run(grab(re.compile(r'\/category\/[^\/]+-\d+\/'), re.compile(
+    r'.+\/category\/[^\/]+-\d+\/'), './parse/categories'))
